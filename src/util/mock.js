3461b45215f4fb1704dfc2aa9b81211087fe76ad
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-06 11:47:37 -0400

    [feature] Created Mock data file

---
+++ src/util/mock.js
@@ -0,0 +1,35 @@
+export const mockServices = [
+    { label: 'Checkup', icon: 'checkup', description: '' },
+    {
+        label: 'General Medicine',
+        icon: 'general-medicine',
+        description:
+            'We offer inpatient and outpatient medical treatment for eye, ear, skin, gastrointestinal, respiratory, cardiac, urogenital, renal and hepatic diseases.',
+    },
+    {
+        label: 'Vaccinations',
+        icon: 'vaccination',
+        description:
+            'Pet Hospital offers vaccination programs consistent with the American Association of Animal Hospital (AAHA) vaccine guidelines. We carry a range of medications to prevent intestinal worms, heartworm, and external parasites such as fleas, ticks and mange',
+    },
+    {
+        label: 'Surgery Care',
+        icon: 'surgery',
+        description:
+            'We provide surgical services ranging from routine spays and neuters to more complex bladder, ear, eye, gastrointestinal, orthopedic and tumour removal procedures. Our veterinarians are experienced surgeons, and we can call upon the services of a mobile specialist if orthopaedic work is needed. We also provide soft tissue surgery and tonometry procedure as well.',
+    },
+    {
+        label: 'Prescription Diets',
+        icon: 'prescription',
+        description:
+            'We can help optimize your pet’s health with a variety of veterinary prescription food.',
+    },
+    {
+        label: 'Specialty Services',
+        icon: 'specialty',
+        description:
+            'In-house pharmacy, nutritional counselling, and pet identification (microchips, etc.)',
+    },
+];
+
+export const mockExperts = [];
