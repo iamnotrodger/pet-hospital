90d4424159f21e807bda6f07d214368de308fd35
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-06 13:33:37 -0400

    [feature] Created Modal components

---
+++ src/component/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Modal = ({
+    isOpen,
+    onClose,
+    fixed = false,
+    overlay = true,
+    children,
+}) => {
+    if (!isOpen) return null;
+
+    return ReactDom.createPortal(
+        <div className='fixed inset-0'>
+            <div
+                className={`absolute inset-0 ${
+                    overlay ? 'bg-black bg-opacity-25' : ''
+                }`}
+                onClick={!fixed && onClose}
+            />
+            {/* <div className='w-full h-full'>{children}</div> */}
+            {children}
+        </div>,
+        document.getElementById('portal'),
+    );
+};
+
+export default Modal;
---
+++ src/component/StandardModal.js
@@ -0,0 +1,21 @@
+import { XCircleIcon } from '@heroicons/react/solid';
+import React from 'react';
+import Modal from './Modal';
+
+const StandardModal = ({ children, isOpen, onClose }) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <div className='h-full flex justify-center items-start p-5 md:pt-10'>
+                <div className='relative w-full max-w-5xl bg-white p-5 rounded-xl shadow-md'>
+                    <XCircleIcon
+                        className='absolute top-0 right-0 w-7 h-7 m-2 text-gray-400 hover:text-black active:text-black'
+                        onClick={onClose}
+                    />
+                    {children}
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default StandardModal;
