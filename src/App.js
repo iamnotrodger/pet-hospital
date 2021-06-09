75515e734f450eff55e7cae83ea40b1794a0d730
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-09 09:52:27 -0400

    [update] Included all components in the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import AppointmentForm from './component/AppointmentForm';
+import Footer from './component/Footer';
+import Hero from './component/Hero';
 
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Hero />
+            <main className='p-8 max-w-screen-xl mx-auto'>
+                <AppointmentForm />
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
