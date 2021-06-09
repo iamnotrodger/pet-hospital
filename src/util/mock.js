741b7f060a99ed35356f923c6a473aa8dd6c0ec7
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-09 11:10:33 -0400

    [update] Mock data information

--- src/util/mock.js
+++ src/util/mock.js
@@ -1,30 +1,41 @@
 export const mockServices = [
-    { label: 'Checkup', icon: 'checkup', description: '' },
     {
+        id: 1,
+        label: 'Checkup',
+        icon: 'checkup',
+        description:
+            'Let us give your pet a healthy start with a new puppy or new kitten examination. We are experienced in pediatric medicine and love seeing new pups and kittens.',
+    },
+    {
+        id: 2,
         label: 'General Medicine',
         icon: 'general-medicine',
         description:
             'We offer inpatient and outpatient medical treatment for eye, ear, skin, gastrointestinal, respiratory, cardiac, urogenital, renal and hepatic diseases.',
     },
     {
+        id: 3,
         label: 'Vaccinations',
         icon: 'vaccination',
         description:
             'Pet Hospital offers vaccination programs consistent with the American Association of Animal Hospital (AAHA) vaccine guidelines. We carry a range of medications to prevent intestinal worms, heartworm, and external parasites such as fleas, ticks and mange',
     },
     {
+        id: 4,
         label: 'Surgery Care',
         icon: 'surgery',
         description:
             'We provide surgical services ranging from routine spays and neuters to more complex bladder, ear, eye, gastrointestinal, orthopedic and tumour removal procedures. Our veterinarians are experienced surgeons, and we can call upon the services of a mobile specialist if orthopaedic work is needed. We also provide soft tissue surgery and tonometry procedure as well.',
     },
     {
+        id: 5,
         label: 'Prescription Diets',
         icon: 'prescription',
         description:
             'We can help optimize your pet’s health with a variety of veterinary prescription food.',
     },
     {
+        id: 6,
         label: 'Specialty Services',
         icon: 'specialty',
         description:
@@ -32,4 +43,37 @@ export const mockServices = [
     },
 ];
 
-export const mockExperts = [];
+export const mockExperts = [
+    {
+        id: 1,
+        photo: '/img/expert-1.jpeg',
+        name: 'Dr. Rebecca Edwards',
+        role: 'Doctor',
+        description:
+            'Aliqua mollit aute dolore elit dolore elit dolore sit ut do veniam ex ipsum mollit. Nostrud velit commodo labore mollit. Dolore cupidatat ipsum et nisi.',
+    },
+    {
+        id: 2,
+        photo: '../img/expert-2.jpeg',
+        name: 'Dr. David Gonksy',
+        role: 'Doctor',
+        description:
+            'Fugiat dolor elit occaecat excepteur ipsum qui occaecat incididunt laborum sunt aliquip mollit dolore. Do do exercitation quis laborum exercitation qui amet. Labore ut culpa ullamco fugiat culpa cillum nisi tempor nostrud.',
+    },
+    {
+        id: 3,
+        photo: '../img/expert-3.jpeg',
+        name: 'Dr. Paula Pingue',
+        role: 'Doctor',
+        description:
+            'Lorem dolore consectetur incididunt minim occaecat id proident. Nostrud eu ex dolor eu tempor proident voluptate excepteur mollit tempor ipsum. Non reprehenderit eiusmod laborum proident laboris.',
+    },
+    {
+        id: 4,
+        photo: '../img/expert-4.jpeg',
+        name: 'Dr. Chinami Ikeda',
+        role: 'Doctor',
+        description:
+            'Incididunt esse labore amet nisi nulla Lorem ipsum sint sunt et aliqua ex culpa. In nulla culpa sunt esse sunt. Minim consectetur pariatur laboris laborum nisi esse ex sint. Cillum reprehenderit aliqua labore occaecat aliquip labore velit tempor.',
+    },
+];
