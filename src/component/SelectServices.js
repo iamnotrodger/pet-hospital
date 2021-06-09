424b1b77d09f153a329b8cedec6f37a9126101b5
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-09 09:51:16 -0400

    [feature] Created SelectServices component

---
+++ src/component/SelectServices.js
@@ -0,0 +1,44 @@
+import { useFormikContext } from 'formik';
+import React from 'react';
+import FormHeaderGroup from './FormHeaderGroup';
+import ServiceList from './ServiceList';
+
+const SelectServices = ({
+    name,
+    label = 'Choose services',
+    list,
+    className,
+}) => {
+    const { values, errors, touched, setFieldValue } = useFormikContext();
+
+    const handleClick = (service) => {
+        if (!service) return;
+
+        const services = values[name];
+        const index = services.findIndex((s) => s.id === service.id);
+
+        if (index > -1) {
+            services.splice(index, 1);
+            setFieldValue(name, [...services]);
+        } else {
+            setFieldValue(name, [...services, service]);
+        }
+    };
+
+    return (
+        <div className={className}>
+            <FormHeaderGroup
+                label={label}
+                error={errors[name]}
+                visible={touched[name]}
+            />
+            <ServiceList
+                list={list}
+                selected={values[name]}
+                onClick={handleClick}
+            />
+        </div>
+    );
+};
+
+export default SelectServices;
